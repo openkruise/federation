cd8bc84ee3515ddc01f33e784b48f569a2b29612
musJinx <musJinx18@outlook.com>
2021-08-05 14:19:20 +0800

    delete useless space

--- controllers/federation/federatedcloneset_controller.go
+++ controllers/federation/federatedcloneset_controller.go
@@ -18,6 +18,7 @@ package federation
 
 import (
 	"context"
+	
 	appsv1alpha1 "github.com/openkruise/kruise-api/apps/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -85,10 +86,3 @@ func (r *FederatedCloneSetReconciler) SetupWithManager(opts *options.Options, mg
 		For(&federationv1alpha1.FederatedCloneSet{}).
 		Complete(r)
 }
-
-
-
-
-
-
-
